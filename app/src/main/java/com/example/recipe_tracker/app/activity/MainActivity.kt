eae918d02b572507bfc343d90327e05ba4bc401f
pineapplleboy <pineapplleboy@gmail.com>
2025-01-05 19:08:28 +0700

    add recipesScreen

--- app/src/main/java/com/example/recipe_tracker/app/activity/MainActivity.kt
+++ app/src/main/java/com/example/recipe_tracker/app/activity/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.recipe_tracker
+package com.example.recipe_tracker.app.activity
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.recipe_tracker.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
